1e5ef950c3848cbb78318c6b648888fb3235f251
Sarvesh Varvatkar <sarveshvarvatkar@Sarveshs-MacBook-Air.local>
2026-02-21 21:56:58 +0530

    feat: fix versel error

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,16 +4,16 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Link } from 'react-router-dom';
 import './Gallery.css';
 
-import img1 from '../assets/images/img1.jpg';
-import img2 from '../assets/images/img2.jpg';
-import img3 from '../assets/images/img3.jpg';
-import img4 from '../assets/images/img4.jpg';
-import img5 from '../assets/images/img5.jpg';
-import img6 from '../assets/images/img6.jpg';
-import img7 from '../assets/images/img7.jpg';
-import img8 from '../assets/images/img8.jpg';
-import img9 from '../assets/images/img9.jpg';
-import img10 from '../assets/images/img10.jpg';
+import img1 from '../assets/images/img1.JPG';
+import img2 from '../assets/images/img2.JPG';
+import img3 from '../assets/images/img3.JPG';
+import img4 from '../assets/images/img4.JPG';
+import img5 from '../assets/images/img5.JPG';
+import img6 from '../assets/images/img6.JPG';
+import img7 from '../assets/images/img7.JPG';
+import img8 from '../assets/images/img8.JPG';
+import img9 from '../assets/images/img9.JPG';
+import img10 from '../assets/images/img10.JPG';
 
 gsap.registerPlugin(ScrollTrigger);
 
