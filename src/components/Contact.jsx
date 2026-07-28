e57abc945da4f5e2252941e7440c1b99dfd6cb04
Sarvesh Varvatkar <varvatkar.sarvesh09@gmail.com>
2026-07-28 23:41:51 +0530

    chore: consistent font

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -86,7 +86,7 @@ function Contact() {
 
                 {/* Resume Download */}
                 <div className="contact-resume-wrapper contact-animate">
-                    <a href="/Resume-3.pdf" download className="contact-resume-btn">
+                    <a href="/Resume.pdf" download className="contact-resume-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
                             <polyline points="7 10 12 15 17 10" />
